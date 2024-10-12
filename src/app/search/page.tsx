b7249f2eca276e52928262ed3997bb485565bde7
Cristian Jucoski <cristian.jucoski@gmail.com>
2024-10-12 20:08:10 -0300

    version 1.0

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useSearchParams } from 'next/navigation'
-import { useEffect, useState } from 'react'
+import { Suspense, useEffect, useState } from 'react'
 import { searchCharacters } from '../service/api'
 import type { Character } from '../types/characters'
 import Navbar from '../components/navbar'
@@ -31,22 +31,27 @@ export default function SearchPage() {
   }, [querySearch])
 
   return (
-    <main className="mx-auto max-w-7xl p-2">
+    <main
+      className="mx-auto   
+ max-w-7xl p-2"
+    >
       <Navbar />
-      <h1 className="w-80 mx-auto text-center ring-2 ring-black m-2 text-3xl font-bold bg-white">
-        Search for <span>&quot;{querySearch}&quot;</span>
-      </h1>
-      {isLoading ? (
-        <div className="mt-10 w-80 mx-auto text-center ring-2 ring-black m-2 text-3xl font-bold bg-white">
-          <h1>Loading...</h1>
-        </div>
-      ) : characters.length ? (
-        <Card characters={characters} />
-      ) : (
-        <div className="mt-10 w-80 mx-auto text-center ring-2 ring-black m-2 text-3xl font-bold bg-white">
-          <h1>Not found</h1>
-        </div>
-      )}
+      <Suspense fallback={<div className="sr-only">Loading...</div>}>
+        <h1 className="w-80 mx-auto text-center ring-2 ring-black m-2 text-3xl font-bold bg-white">
+          Search for <span>&quot;{querySearch}&quot;</span>
+        </h1>
+        {isLoading ? (
+          <div className="mt-10 w-80 mx-auto text-center ring-2 ring-black m-2 text-3xl font-bold bg-white">
+            <h1>Loading...</h1>
+          </div>
+        ) : characters.length ? (
+          <Card characters={characters} />
+        ) : (
+          <div className="mt-10 w-80 mx-auto text-center ring-2 ring-black m-2 text-3xl font-bold bg-white">
+            <h1>Not found</h1>
+          </div>
+        )}
+      </Suspense>
     </main>
   )
 }
